d3aac75047412171f72f99f44e3f89817056dde6
bekirglr <bbekirglr@gmail.com>
2022-08-19 23:19:26 +0300

    A simple age calculator that can run on console

--- custom.js
+++ custom.js
@@ -1,2 +1,11 @@
-var zaman = Date();
-console.log(zaman);
+var dogum_gun = prompt("Lütfen Gün Giriniz : ");
+var dogum_ay = prompt("Lütfen AY Giriniz : ");
+var dogum_yil = prompt("Lütfen Yıl Giriniz : ");
+
+var gun = new Date().getDay();
+var ay = new Date().getMonth();
+var yil = new Date().getFullYear();
+
+var yas = yil-dogum_yil;
+
+document.writeln(yas);
